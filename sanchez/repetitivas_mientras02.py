753f1cc20484ff7568a4ec61c1bbf2c04060b008
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-24 10:24:13 -0500

    se agrego la carpeta python file repetitivas_mientras02 en el directorio sanchez

--- sanchez/repetitivas_mientras02.py
+++ sanchez/repetitivas_mientras02.py
@@ -1,12 +1,11 @@
-import os
-# MOSTRAR VALORES
+# MOSTRAR LA NOTA DE INGLES ENTRE (0 Y 100) SI NO ES VALIDA "PEDIR OTRA VES"
 nota_de_ingles=0
 nota_invalida=True
 
 # INPUT
+import os
 nota_de_ingles=int(os.sys.argv[1])
 
-# MOSTRAR LA NOTA DE INGLES ENTRE (0 Y 100) SI NO ES VALIDA "PEDIR OTRA VES"
 while(nota_invalida):
     nota_de_ingles=int(input("escriba otra vez la nota: "))
     nota_invalida=(nota_de_ingles<=0 or nota_de_ingles>=100)
