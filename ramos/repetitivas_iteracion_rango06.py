3dc9f5692d984d1ca0718858b25be0b34b3ba363
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-24 12:35:06 -0500

    se agrego la carpeta python file repetitivas_iteracion_rango06 al directorio ramos continuacion de repetitivas_iteracion_rango05 del directorio sanchez

---
+++ ramos/repetitivas_iteracion_rango06.py
@@ -0,0 +1,14 @@
+import os
+# Imprimir los numeros del 20 al 100
+
+#input
+y=int(os.sys.argv[1])
+z=int(os.sys.argv[2])
+
+#iterador
+for i in range(y,z):
+    print(i)
+
+#fin_iterador_en_rango
+
+print("Fin del bucle")
