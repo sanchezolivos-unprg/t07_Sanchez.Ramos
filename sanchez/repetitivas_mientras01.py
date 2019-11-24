0e89bd13070cfbb46475d85fdb34ec24506453c3
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-24 10:09:07 -0500

    se agrego la carpeta python file repetitivas_mientras01

---
+++ sanchez/repetitivas_mientras01.py
@@ -0,0 +1,20 @@
+# MOSTRAR EL NUMERO MIENTRAS SEA POSITIVO, NEGATIVO O IGUAL A CERO
+numero=0
+respuesta="si"
+
+# input
+import os
+numero=int(os.sys.argv[1])
+respuesta=os.sys.argv[2]
+
+while(respuesta=="si"):
+    numero=int(input("digite un numero: "))
+    if(numero>0):
+        print("el numero ingresado es posotivo")
+    elif(numero<0):
+        print("el numero ingresado es negativo")
+    else:
+        print("el numero ingresado es igual a cero")
+    respuesta=input("¿Quieres ingresar otro numero? <si - no> ")
+#fin_while
+print("fin del bucle")
