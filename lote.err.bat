ad3c1464753a524983107579c67d4b73673d7088
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-24 13:11:26 -0500

    se agrego el file lote.err.bat de sanchez y ramos con error

---
+++ lote.err.bat
@@ -0,0 +1,81 @@
+python sanchez/repetitivas_mientras01.py -4 no
+python sanchez/repetitivas_mientras01.py -8 no
+python sanchez/repetitivas_mientras01.py -10 no
+
+python sanchez/repetitivas_mientras02.py -36
+python sanchez/repetitivas_mientras02.py 150
+python sanchez/repetitivas_mientras02.py 200
+
+python sanchez/repetitivas_mientras03.py H
+python sanchez/repetitivas_mientras03.py K
+
+python sanchez/repetitivas_mientras04.py 2.5
+python sanchez/repetitivas_mientras04.py 95.8
+python sanchez/repetitivas_mientras04.py 7.8
+
+python sanchez/repetitivas_mientras05.py "tambor"
+python sanchez/repetitivas_mientras05.py "piano"
+python sanchez/repetitivas_mientras05.py "bateria"
+
+python ramos/repetitivas_mientras06.py 200
+python ramos/repetitivas_mientras06.py 300
+python ramos/repetitivas_mientras06.py -20
+
+python ramos/repetitivas_mientras07.py 50
+python ramos/repetitivas_mientras07.py 30
+python ramos/repetitivas_mientras07.py 100
+
+python ramos/repetitivas_mientras08.py 45546
+
+python ramos/repetitivas_mientras09.py verde
+python ramos/repetitivas_mientras09.py morado
+python ramos/repetitivas_mientras09.py celeste
+
+python ramos/repetitivas_mientras10.py arroz.g arroz.e
+python ramos/repetitivas_mientras10.py arroz.f arroz.l
+
+python sanchez/repetitivas_iteracion01.py "mnpq"
+
+python sanchez/repetitivas_iteracion02.py "juytr"
+python sanchez/repetitivas_iteracion02.py "trsea"
+python sanchez/repetitivas_iteracion02.py "gytda"
+
+python sanchez/repetitivas_iteracion03.py "abcdef"
+python sanchez/repetitivas_iteracion03.py "123456"
+python sanchez/repetitivas_iteracion03.py "ouzxew"
+
+python sanchez/repetitivas_iteracion04.py "gdtfrsc"
+python sanchez/repetitivas_iteracion04.py "987tgvc"
+python sanchez/repetitivas_iteracion04.py "bnifydt"
+
+python sanchez/repetitivas_iteracion05.py "qrn4562"
+python sanchez/repetitivas_iteracion05.py "12350zx"
+python sanchez/repetitivas_iteracion05.py "ou45689"
+
+python ramos/repetitivas_iteracion06.py DSFD
+
+python ramos/repetitivas_iteracion07.py HDHF
+
+python ramos/repetitivas_iteracion08.py REGH
+
+python ramos/repetitivas_iteracion09.py REGR
+
+python ramos/repetitivas_iteracion10.py 1566
+
+python sanchez/repetitivas_iteracion_rango01.py 8.9
+python sanchez/repetitivas_iteracion_rango01.py 6.0
+python sanchez/repetitivas_iteracion_rango01.py 2.5
+
+python sanchez/repetitivas_iteracion_rango02.py 3.6
+python sanchez/repetitivas_iteracion_rango02.py 8.7
+python sanchez/repetitivas_iteracion_rango02.py 4.8
+
+python ramos/repetitivas_iteracion_rango06.py 50 500
+
+python ramos/repetitivas_iteracion_rango07.py 600
+
+python ramos/repetitivas_iteracion_rango08.py 800
+
+python ramos/repetitivas_iteracion_rango09.py 100
+
+python ramos/repetitivas_iteracion_rango10.py 20 900
