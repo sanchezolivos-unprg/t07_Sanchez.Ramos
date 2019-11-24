a60a62f0497ca244273f38637f672b1062504b5b
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-24 10:53:48 -0500

    se agrego la carpeta python file repetitivas_iteracion_rango03 al directorio sanchez

---
+++ sanchez/repetitivas_iteracion_rango03.py
@@ -0,0 +1,12 @@
+# Imprimir los 30 primeros numeros
+
+# input
+import os
+int(os.sys.argv[1])
+
+# iterador en rango
+for i in range(30):
+    print(i+1)
+# fin_iterador_en_rango
+
+print("Fin del bucle")
