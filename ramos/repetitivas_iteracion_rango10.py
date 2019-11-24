5abb4c171fd094ecaaca61d1f0d63e1347068b24
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-24 12:42:34 -0500

    se agrego la carpeta python file repetitivas_iteracion_rango10 al directorio ramos

---
+++ ramos/repetitivas_iteracion_rango10.py
@@ -0,0 +1,14 @@
+import os
+# Imprimir los numeros del 100 al 200
+
+#input
+y=int(os.sys.argv[1])
+z=int(os.sys.argv[2])
+
+#iterador
+for i in range(y,z):
+    print(i)
+
+#fin_iterador_en_rango
+
+print("Fin del bucle")
