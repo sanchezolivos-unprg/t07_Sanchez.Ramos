cdf9b036409c29988e5ad41c527a998fd4f176d6
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-24 12:39:50 -0500

    se agrego la carpeta python file repetitivas_iteracion_rango08 al directorio ramos

---
+++ ramos/repetitivas_iteracion_rango08.py
@@ -0,0 +1,14 @@
+import os
+#calcular la suma de todos los numeros del 20 al 2000
+#input
+z=int(os.sys.argv[1])
+i=0
+suma=0
+#bucle
+for i in range(z):
+    suma +=i
+    i += 1
+print(suma)
+#fin_iterador_en_rango
+
+print("Fin del bucle")
