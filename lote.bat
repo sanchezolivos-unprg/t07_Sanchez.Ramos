7c765c77441c5998c4c0e37b8c97537821b7091b
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-24 13:00:59 -0500

    se agrego el file lote.bat de sanchez y ramos que son correctos

---
+++ lote.bat
@@ -0,0 +1,85 @@
+python sanchez/repetitivas_mientras01.py 4 si
+python sanchez/repetitivas_mientras01.py -5 si
+python sanchez/repetitivas_mientras01.py 0 si
+
+python sanchez/repetitivas_mientras02.py 10
+python sanchez/repetitivas_mientras02.py 8
+python sanchez/repetitivas_mientras02.py 56
+
+python sanchez/repetitivas_mientras03.py B
+python sanchez/repetitivas_mientras03.py R
+
+python sanchez/repetitivas_mientras04.py 15.5
+python sanchez/repetitivas_mientras04.py 45.8
+python sanchez/repetitivas_mientras04.py 36.6
+
+python sanchez/repetitivas_mientras05.py "ukelele"
+python sanchez/repetitivas_mientras05.py "violin"
+python sanchez/repetitivas_mientras05.py "guitarra"
+
+python ramos/repetitivas_mientras06.py 20
+python ramos/repetitivas_mientras06.py 50
+python ramos/repetitivas_mientras06.py 8
+
+python ramos/repetitivas_mientras07.py 5
+python ramos/repetitivas_mientras07.py 8
+python ramos/repetitivas_mientras07.py 4
+
+python ramos/repetitivas_mientras08.py 12345
+
+python ramos/repetitivas_mientras09.py rojo
+python ramos/repetitivas_mientras09.py azul
+python ramos/repetitivas_mientras09.py amarillo
+
+python ramos/repetitivas_mientras10.py arroz.p arroz.c
+python ramos/repetitivas_mientras10.py arroz.p arroz.c
+
+python sanchez/repetitivas_iteracion01.py "wxyz"
+
+python sanchez/repetitivas_iteracion02.py "12345"
+
+python sanchez/repetitivas_iteracion03.py "pqrstm"
+
+python sanchez/repetitivas_iteracion04.py "6543210"
+
+python sanchez/repetitivvas_iteracion05.py "ltfswmh"
+
+python ramos/repetitivas_iteracion06.py JFAX
+
+python ramos/repetitivas_iteracion07.py FWYU
+
+python ramos/repetitivas_iteracion08.py TITO
+
+python ramos/repetitivas_iteracion09.py GDTR
+
+python ramos/repetitivas_iteracion10.py 1569
+
+python sanchez/repetitivas_iteracion_rango01.py 4
+python sanchez/repetitivas_iteracion_rango01.py 8
+python sanchez/repetitivas_iteracion_rango01.py 6
+
+python sanchez/repetitivas_iteracion_rango01.py -9
+python sanchez/repetitivas_iteracion_rango01.py 16
+python sanchez/repetitivas_iteracion_rango01.py 7
+
+python sanchez/repetitivas_iteracion_rango02.py 0
+
+python sanchez/repetitivas_iteracion_rango02.py 4
+
+python sanchez/repetitivas_iteracion_rango03.py 1
+
+python sanchez/repetitivas_iteracion_rango04.py 7
+
+python sanchez/repetitivas_iteracion_rango05.py 1 9
+python sanchez/repetitivas_iteracion_rango05.py 3 25
+python sanchez/repetitivas_iteracion_rango05.py 0 19
+
+python ramos/repetitivas_iteracion_rango06.py 20 100
+
+python ramos/repetitivas_iteracion_rango07.py 1001
+
+python ramos/repetitivas_iteracion_rango08.py 2000
+
+python ramos/repetitivas_iteracion_rango09.py 100
+
+python ramos/repetitivas_iteracion_rango10.py 100 200
