2851022eb4a123cf6be43c803c38f8e79606b719
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-11-24 10:52:42 -0500

    se agrego la carpeta python file repetitivas_iteracion_rango02 al directorio sanchez

---
+++ sanchez/repetitivas_iteracion_rango02.py
@@ -0,0 +1,11 @@
+# imprimir los numeros entre 0-50 de 5 en 5
+
+# input
+import os
+numero=int(os.sys.argv[1])
+
+# iterador en rango
+for numero in range(0, 51, 5):
+    print(numero)
+# fin_iterador_en_rango
+print("fin del bucle")
